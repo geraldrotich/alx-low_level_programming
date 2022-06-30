c40e0bdea46682e8e3a9844c5a8cb0f719c72859
Gerald <rottych@gmail.com>
2022-06-30 15:47:59 -0700

    File types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,23 @@
-#include<stdio.h>
 /**
-* main - Entry point
+* main - print the string in the put function
 *
-*
-* Return: 0 number
+* Description: using the main function
+* this program prints all variable sizes
+* Return: 0
 */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
-	return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float int: %ld bytes(s)\n", sizeof (f));
+
+return(0);
 }
-
-
